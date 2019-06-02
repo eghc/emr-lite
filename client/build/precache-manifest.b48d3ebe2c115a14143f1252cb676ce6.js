fd693b3436c32c49a40e45af7974cd4245c6b66e
eghc <eringraceharris@gmail.com>
2019-06-02 16:46:47 -0700

    reverted again to same checkpoint and updated start in package

---
+++ client/build/precache-manifest.b48d3ebe2c115a14143f1252cb676ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f646c7b7df56b725ea5ca3eff6e66d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c930792c783b5c08cb84",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "f8f1bf816f6d643ae898",
+    "url": "/static/css/main.ac36c0ee.chunk.css"
+  },
+  {
+    "revision": "c930792c783b5c08cb84",
+    "url": "/static/js/2.e48a167e.chunk.js"
+  },
+  {
+    "revision": "f8f1bf816f6d643ae898",
+    "url": "/static/js/main.573784d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
