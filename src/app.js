3662989d364859cb5ddb7129985aa6be2db7e0bf
eghc <eringraceharris@gmail.com>
2019-06-02 17:00:25 -0700

    updated server.js file to server build:

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
 appConfig.init();
 routeConfig.init(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join('./client/build')));
+app.get('*', function(req, res) {    res.sendFile(path.join('./client/build', 'index.html'));  });
 
 module.exports = app;
