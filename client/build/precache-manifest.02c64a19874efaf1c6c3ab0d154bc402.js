edfa9c0400b83b4db4d327d32f3b87591283c67a
eghc <eringraceharris@gmail.com>
2019-06-03 21:52:27 -0700

    welp come back to this, but front end is broken

---
+++ client/build/precache-manifest.02c64a19874efaf1c6c3ab0d154bc402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81b3a5246e0bd57c4e1b84470892097",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2fc158e45c2a1bcb706",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "4cd90fe9b3bec84e1bf9",
+    "url": "/static/css/main.ac36c0ee.chunk.css"
+  },
+  {
+    "revision": "a2fc158e45c2a1bcb706",
+    "url": "/static/js/2.d6ea5289.chunk.js"
+  },
+  {
+    "revision": "4cd90fe9b3bec84e1bf9",
+    "url": "/static/js/main.6fa5b40e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
